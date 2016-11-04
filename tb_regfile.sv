19c211525d3c2dc5414a245cf060104029956cd2
Leandro Lubrico <Leandro Lubrico>
2016-11-04 04:09:34 -0700

    Added comments to regfile testbench

--- tb_regfile.sv
+++ tb_regfile.sv
@@ -23,29 +23,39 @@ module tb_regfile();
 		clk = 0;
 		#100;
 		
+		// Write 63 to address 2
+		// Should result in register 2 being 8'b00111111
 		w_addr = 2;
 		w_data = 63;
 		r_or_w = 1;
 		
 		#100;
 		
+		// Read address 1 and 2
+		// Data 1 should be 63 and Data 2 should be 0
 		r_addr1 = 2;
 		r_addr2 = 1;
 		r_or_w = 0;
 		
 		#100;
 		
+		// Write 31 to address 4
+		// Should result in register 4 being 8'b00011111
 		w_addr = 4;
 		w_data = 31;
 		r_or_w = 1;
 		
 		#100;
 		
+		// Read registers 4 and 2
+		// Data 1 should be 31 and Data 2 should be 63
 		r_addr1 = 4;
 		r_addr2 = 2;
 		r_or_w = 0;
 		
 		#100;
+		// Done testing reading and writing to register file
+		// Also tested being able to either read or write, not at the same time
 		
 		
 	end
