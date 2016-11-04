404133979795a5d4995a0908d9649dbdd62a1910
Brandon Chin <Brandon Chin>
2016-11-03 22:39:53 -0700

    add fetch unit

---
+++ fetch_tb.sv
@@ -0,0 +1,11 @@
+module fetch_tb;
+
+	// inputs
+	bit clock;
+	bit overflow;
+	bit bno;
+	
+	// output
+	output [8:0] instruction;
+	
+endmodule
---
+++ im_test_tb.sv
@@ -0,0 +1,25 @@
+module im_test_tb;
+
+	// input
+	logic [4:0] address;
+	
+	// output
+	wire [8:0] instruction;
+	
+	// instantiate UUT
+	im_test uut (
+		.address,
+		.instruction
+	);
+	
+	bit clock;
+	
+	initial begin
+		#100ns;
+		#25ns address = 4;
+		#25ns address = 17;
+		#25ns address = 12;
+		#25ns address = 28;
+	end
+	
+endmodule
